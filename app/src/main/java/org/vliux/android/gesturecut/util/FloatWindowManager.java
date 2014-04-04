6a49e525773a8593953464bd746beb837e9c8371
曜山 <simpsons.liux@alipay.com>
2014-04-04 15:36:10 +0800

    replace float window icon

--- app/src/main/java/org/vliux/android/gesturecut/util/FloatWindowManager.java
+++ app/src/main/java/org/vliux/android/gesturecut/util/FloatWindowManager.java
@@ -10,8 +10,6 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
 
-import org.vliux.android.gesturecut.AppConstant;
-
 /**
  * Created by vliux on 10/15/13.
  */
